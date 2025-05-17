8ad0792049634cb9cf3e9158e90c8b6f05d84c88
Jason Mei <jason.mei55@qmail.cuny.edu>
2025-05-17 01:14:47 -0400

    menu bar

---
+++ src/app/components/Menubar.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function Menubar() {
+    return (
+      <>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <a href='https://expense-green-alpha.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
+          </li>
+          <li>
+            <a href='https://customer-weld.vercel.app/' target='_blank'>Midterm Part II Site</a>
+          </li>
+        </ul>
+      </>
+    )
+  }
+   
