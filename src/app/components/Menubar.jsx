ccb2d067630fedac1715cb20a95c4448e150a624
Jason Mei <jason.mei55@qmail.cuny.edu>
2025-05-19 14:29:12 -0400

    added tailwind

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -3,21 +3,42 @@ import Link from 'next/link'
 export default function Menubar() {
     return (
       <>
-        <ul>
-          <li>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <a href='https://expense-sable-chi.vercel.app' target='_blank'>Midterm Part I - Expense</a>
-          </li>
-          <li>
-            <a href='https://customer-weld.vercel.app/' target='_blank'>Midterm Part II Site</a>
-          </li>
-
-          <li>
-            <Link href='/faq'>FAQ</Link>
-            </li>
-        </ul>
+       <ul className="space-y-1 p-4 bg-gray-50 rounded-lg shadow-md text-center">
+  <li>
+    <Link 
+      href='/' 
+      className="block px-4 py-2 font-bold italic text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded transition-colors duration-200"
+    >
+      Home
+    </Link>
+  </li>
+  <li>
+    <a 
+      href='https://expense-sable-chi.vercel.app' 
+      target='_blank'
+      className="block px-4 py-2 font-bold italic text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded transition-colors duration-200"
+    >
+      Midterm Part I - Expense
+    </a>
+  </li>
+  <li>
+    <a 
+      href='https://customer-weld.vercel.app/' 
+      target='_blank'
+      className="block px-4 py-2 font-bold italic text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded transition-colors duration-200"
+    >
+      Midterm Part II - Site
+    </a>
+  </li>
+  <li>
+    <Link 
+      href='/faq'
+      className="block px-4 py-2 font-bold italic text-gray-700 hover:bg-blue-50 hover:text-blue-600 rounded transition-colors duration-200"
+    >
+      FAQ
+    </Link>
+  </li>
+</ul>
       </>
     )
   }
